b70a84c3de199284f271169312ea1c90c5ce3bdd
NikitaKothari <nikitakothari21@gmail.com>
2020-05-07 16:43:47 -0700

    pool added

--- myapp/redis.py
+++ myapp/redis.py
@@ -2,4 +2,5 @@ import redis
 
 from django.conf import settings
 
-client = redis.from_url(settings.REDIS_URL, decode_responses=True)
+POOL = redis.ConnectionPool.from_url(settings.REDIS_URL)
+client = redis.from_url(connection_pool=POOL, decode_responses=True)
