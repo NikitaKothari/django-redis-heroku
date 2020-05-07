66eb31c9fd341b1663dfc4fa937542e55c730f0a
NikitaKothari <nikitakothari21@gmail.com>
2020-05-07 16:52:12 -0700

    redis

--- myapp/redis.py
+++ myapp/redis.py
@@ -3,4 +3,4 @@ import redis
 from django.conf import settings
 
 POOL = redis.ConnectionPool.from_url(settings.REDIS_URL)
-client = redis.from_url(connection_pool=POOL, decode_responses=True)
+client = redis.Redis(connection_pool=POOL, decode_responses=True)
