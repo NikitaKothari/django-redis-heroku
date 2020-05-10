e6793a2f1eba6147cf2fe2c4644c3bf9aa666882
NikitaKothari <nikitakothari21@gmail.com>
2020-05-10 15:51:09 -0700

    test

--- myapp/pool.py
+++ myapp/pool.py
@@ -6,4 +6,4 @@ print("******************************************")
 print(settings.LIMIT)
 print("******************************************")
 
-POOL = redis.ConnectionPool.from_url(settings.REDIS_URL, max_connections=settings.LIMIT)
+POOL = redis.ConnectionPool.from_url(settings.REDIS_URL, max_connections=5)
