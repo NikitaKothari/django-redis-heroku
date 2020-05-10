5c0d3724fa87ae204b31ec1bd42723ef97f21940
NikitaKothari <nikitakothari21@gmail.com>
2020-05-10 13:59:33 -0700

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -113,7 +113,7 @@ BROKER_POOL_LIMIT = os.environ.get('BROKER_POOL_LIMIT', 10)
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', BROKER_URL)
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', BROKER_URL)
 
-LIMIT = os.environ.get('LIMIT', 10)
+LIMIT = os.environ.get('LIMIT', 5)
 
 
 # Configure Django App for Heroku.
