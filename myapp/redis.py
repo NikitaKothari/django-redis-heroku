eb21504a5ec03e8ae9cce6ee9b57e3ba033bc5ca
NikitaKothari <nikitakothari21@gmail.com>
2020-05-19 10:34:51 -0700

    Build pack changes

--- myapp/redis.py
+++ myapp/redis.py
@@ -1,4 +1,4 @@
 import redis
-from myapp.pool import POOL
+from django.conf import settings
 
-client = redis.Redis(host="localhost", port=6201, password='p39d87357fb2861c9b82d0f3e682ffd01d56ae6f26ed2bdd84bbd65bfaecbd1bf')
+client = redis.from_url(settings.REDIS_URL)
