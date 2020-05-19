0256e7846ffc1110e311c10745025655f77daf9b
NikitaKothari <nikitakothari21@gmail.com>
2020-05-19 15:49:24 -0700

    Test

--- myapp/redis.py
+++ myapp/redis.py
@@ -1,4 +1,12 @@
 import redis
 from django.conf import settings
 
+import structlog
+log = structlog.get_logger()
+
+
+log.info("*******************************")
+log.info(REDIS_URL=settings.REDIS_URL)
+log.info("*******************************")
+
 client = redis.from_url(settings.REDIS_URL)
