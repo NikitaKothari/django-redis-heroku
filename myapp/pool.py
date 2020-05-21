1ff3cfbf4e4f36c472e30b3836a2d34266450276
NikitaKothari <nikitakothari21@gmail.com>
2020-05-21 14:24:24 -0700

    Test

--- myapp/pool.py
+++ myapp/pool.py
@@ -2,4 +2,4 @@ import redis
 
 from django.conf import settings
 
-#POOL = redis.ConnectionPool.from_url(settings.REDIS_URL, max_connections=settings.LIMIT)
+POOL = redis.ConnectionPool.from_url(settings.REDIS_URL, max_connections=settings.LIMIT, timeout=5)
