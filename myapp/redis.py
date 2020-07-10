5fc8c8fc8cf09f7f0b002efdee1f51858e82bebc
NikitaKothari <nikitakothari21@gmail.com>
2020-07-10 12:41:40 -0700

    Test

--- myapp/redis.py
+++ myapp/redis.py
@@ -1,5 +1,12 @@
 import redis
 from django.conf import settings
+import structlog
+
+log = structlog.get_logger()
+log.info("*******************")
+log.info(settings.REDIS_URL)
+log.info("*******************")
+
 
 POOL = redis.ConnectionPool.from_url(settings.REDIS_URL, max_connections=settings.LIMIT)
 client = redis.Redis(connection_pool=POOL, decode_responses=True)
