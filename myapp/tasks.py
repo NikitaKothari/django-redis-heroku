6e4371c9df082bfc43fc4546c2c83a2d3bf06838
NikitaKothari <nikitakothari21@gmail.com>
2020-07-10 14:13:35 -0700

    Disabled taskss

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -19,7 +19,7 @@ def counter():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter1():
     log.info("counter1")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -28,7 +28,7 @@ def counter1():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter2():
     log.info("counter2")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -37,7 +37,7 @@ def counter2():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter3():
     log.info("counter3")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -46,7 +46,7 @@ def counter3():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter4():
     log.info("counter4")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -55,7 +55,7 @@ def counter4():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter5():
     log.info("counter5")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -64,7 +64,7 @@ def counter5():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter6():
     log.info("counter6")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -73,7 +73,7 @@ def counter6():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter7():
     log.info("counter7")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -82,7 +82,7 @@ def counter7():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter8():
     log.info("counter8")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -91,7 +91,7 @@ def counter8():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter9():
     log.info("counter9")
     instance, created = MyModel.objects.get_or_create(id=1)
@@ -100,7 +100,7 @@ def counter9():
     instance.save()
 
 
-@periodic_task(run_every=crontab(minute="*"))
+#@periodic_task(run_every=crontab(minute="*"))
 def counter10():
     log.info("counter10")
     instance, created = MyModel.objects.get_or_create(id=1)
