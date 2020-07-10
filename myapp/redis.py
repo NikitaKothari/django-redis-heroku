445a69dfdf87d503e5c122747a2199ee8acbb604
NikitaKothari <nikitakothari21@gmail.com>
2020-07-10 10:09:16 -0700

    Removed timeout

--- myapp/redis.py
+++ myapp/redis.py
@@ -3,4 +3,3 @@ from django.conf import settings
 
 POOL = redis.ConnectionPool.from_url(settings.REDIS_URL, max_connections=settings.LIMIT)
 client = redis.Redis(connection_pool=POOL, decode_responses=True)
-client.config_set("timeout", 100)
