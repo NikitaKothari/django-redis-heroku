9a5875cf5b6c1f4732eb6abe669d01987193c388
NikitaKothari <nikitakothari21@gmail.com>
2020-07-10 13:00:31 -0700

    Middleware

--- myapp/redis.py
+++ myapp/redis.py
@@ -1,12 +1,5 @@
 import redis
 from django.conf import settings
-import structlog
-
-log = structlog.get_logger()
-log.info("*******************")
-log.info(settings.REDIS_URL)
-log.info("*******************")
-
 
 POOL = redis.ConnectionPool.from_url(settings.REDIS_URL)
 client = redis.Redis(connection_pool=POOL, decode_responses=True)
