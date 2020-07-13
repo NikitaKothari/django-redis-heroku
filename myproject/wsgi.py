83f7b50eeeb142afa8cd06d698faea164aadf778
NikitaKothari <nikitakothari21@gmail.com>
2020-07-13 16:34:30 -0700

    Test

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 application = get_wsgi_application()
+
+# required to make nginx serve traffic. see https://github.com/heroku/heroku-buildpack-nginx#applicationdyno-coordination
+from pathlib import Path
+
+Path("/tmp/app-initialized").touch()
