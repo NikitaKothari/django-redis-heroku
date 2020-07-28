712cb1126201fa912eb45cc6cd810b85910e76d5
NikitaKothari <nikitakothari21@gmail.com>
2020-07-28 15:38:07 -0700

    Test

--- myproject/celery.py
+++ myproject/celery.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
 import os
+from django.apps import apps
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 app = Celery("screenmatter")
 
 app.config_from_object("django.conf:settings")
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
