895a8b220905987ea4f4e132ddf9b125552b0041
NikitaKothari <nikitakothari21@gmail.com>
2020-08-26 14:05:15 -0700

    test

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -8,6 +8,7 @@ log = structlog.get_logger()
 from celery import Task
 import celery
 from myapp.redis import client
+from django.conf import settings
 
 
 class MyTask(Task):
@@ -37,6 +38,7 @@ class Test:
 def counter_task():
     client.set("Nikita", 5)
     log.info("---------From Redis----------")
+    log.info(settings.REDIS_URL)
     log.info(client.get("Nikita"))
     log.info("---------To Redis----------")
     Test().counter()
